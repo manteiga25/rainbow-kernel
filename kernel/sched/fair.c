0417f679f59ae356c270695723b9ae2d949ffdc5
manteiga25 <80206082+manteiga25@users.noreply.github.com>
2022-01-15 21:13:36 +0000

    kernel/sched: reduce latency [Pafcholini]
    Signed-off-by: HolyAngel <slverwolf@gmail.com>
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -35,10 +35,10 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  *
- * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 5ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency			= 6000000ULL;
-unsigned int normalized_sysctl_sched_latency		= 6000000ULL;
+unsigned int sysctl_sched_latency			= 5000000ULL;           // 6000000ULL
+unsigned int normalized_sysctl_sched_latency		= 5000000ULL;           // 6000000ULL
 
 /*
  * Enable/disable honoring sync flag in energy-aware wakeups.
@@ -66,7 +66,7 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_L
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
- * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 0.50 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
 unsigned int sysctl_sched_min_granularity		= 500000ULL;            // 750000ULL
 unsigned int normalized_sysctl_sched_min_granularity	= 500000ULL;            // 750000ULL
@@ -115,7 +115,7 @@ int __weak arch_asym_cpu_priority(int cpu)
  * to consumption or the quota being specified to be smaller than the slice)
  * we will always only issue the remaining available time.
  *
- * (default: 5 msec, units: microseconds)
+ * (default: 4 msec, units: microseconds)
  */
 unsigned int sysctl_sched_cfs_bandwidth_slice		= 4000UL;   // 5000UL
 #endif
