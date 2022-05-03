7df57c5254f771617a743bd897b250d2b8e1ee1f
manteiga25 <alexmykhaylo@gmail.com>
2022-05-03 17:51:14 +0100

    import more alterations

--- fs/exec.c
+++ fs/exec.c
@@ -1831,7 +1831,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-if (is_global_init(current->parent)) {
+        if (is_global_init(current->parent)) {
 		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
 			zygote32_sig = current->signal;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
